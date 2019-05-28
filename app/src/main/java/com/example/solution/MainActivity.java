e1352ff6d278fc97e9752ae91e5fb918ee4ea7e0
mmaina8 <joyymuthoni8@gmail.com>
2019-05-28 23:27:29 +0300

    First commit

--- app/src/main/java/com/example/solution/MainActivity.java
+++ app/src/main/java/com/example/solution/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,7 +30,15 @@ public class MainActivity extends AppCompatActivity {
         });
 
         TextView tvHello = findViewById(R.id.tvHello);
-        tvHello.setText("I am a baller");
+        tvHello.setText("God is good");
+
+        Button btnViewNote = findViewById(R.id.btnViewNote);
+        btnViewNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(), ViewNote.class));
+            }
+        });
     }
 
     @Override
